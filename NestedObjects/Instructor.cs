147b37c2855235f3910181062cf9ddc694d411a1
lampert309 <lampert309@gmail.com>
2019-01-16 13:26:56 -0800

    Added comments to all variables inside of Instructor class

--- NestedObjects/Instructor.cs
+++ NestedObjects/Instructor.cs
@@ -6,8 +6,20 @@ using System.Threading.Tasks;
 
 namespace NestedObjects {
     class Instructor {
+
+        /// <summary>
+        /// full legal name of the Instructor
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// work email address for the instructor
+        /// </summary>
         public string Email { get; set; }
+
+        /// <summary>
+        /// office phone number for the instructor
+        /// </summary>
         public string PhoneNum { get; set; }
     }
 }
