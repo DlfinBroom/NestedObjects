4c3ef7478e0bb1b4492bde5a3a536b7ee4831cf9
lampert309 <lampert309@gmail.com>
2019-01-16 13:46:28 -0800

    Created Course class and created Title, Credits, Description, and Hours variables inside of Cources along with documentation

---
+++ NestedObjects/Course.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects {
+    class Course {
+        /// <summary>
+        /// The name of the class
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// The number of credits this class is worth
+        /// </summary>
+        public string Credits { get; set; }
+
+        /// <summary>
+        /// The description of the class
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// The number of hours the course is in one quarter
+        /// </summary>
+        public string Hours { get; set; }
+    }
+}
